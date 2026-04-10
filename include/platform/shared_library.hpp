4f48f2a66066c159436dd7c594961ab7123b0af3
Gabriele Cocchieri <gabriele.cocchieri@gmail.com>
2026-04-10 22:33:02 +0200

    Fixed macos crashing on Launch
    A stupid if statement on Graphics API check was wrong.

--- include/platform/shared_library.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-using LibraryHandle = void*;
-
-LibraryHandle LoadSharedLibrary(const std::string& path);
-void* GetLibrarySymbol(LibraryHandle handle, const std::string& symbolName);
-void UnloadSharedLibrary(LibraryHandle handle);
